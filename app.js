103e17dd874b05a5f0147872d9e03cd37a4afb01
Wisdom Sackey Asigbey <wisdomasigbey55@gmail.com>
2025-09-10 19:33:32 +0300

    initial connect, updates, and testings

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 // async errors
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
+
+const conectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -16,6 +20,8 @@ app.get("/", (req, res) => {
   res.send(`<h1>Store API</h1><a href="/api/v1/products">products route</a>`);
 });
 
+app.use("/api/v1/products", productsRouter);
+
 // product route
 
 app.use(notFoundMiddleware);
@@ -26,6 +32,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // connect DB
+    await conectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
