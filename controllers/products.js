67bc3998079321a9ad53ced489907a1c9331a692
Wisdom Sackey Asigbey <wisdomasigbey55@gmail.com>
2025-09-12 18:34:22 +0300

    Query params initial based on featured property

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,19 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const product = await Product.find({ featured: true });
-  res.status(200).json({ product });
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "product route" });
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryObject);
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
