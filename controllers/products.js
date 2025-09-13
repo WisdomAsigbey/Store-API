ff2e53aaa193d4e7e5e4afcb9516fc16feaae95f
Wisdom Sackey Asigbey <wisdomasigbey55@gmail.com>
2025-09-13 19:46:55 +0300

    Pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+  const products = await Product.find({});
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
   // destructure from query
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   // empty var
   const queryObject = {};
   // filters
@@ -19,7 +19,11 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  // console.log(queryObject);
+  console.log(queryObject);
+  // numeric filters
+  if (numericFilters) {
+    console.log(numericFilters);
+  }
 
   let result = Product.find(queryObject);
   // sort
@@ -36,6 +40,11 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  // pagination;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
